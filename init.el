2d94c8ca1a91b22a3934e4d2104bffc06d8aef39
Ole-Kenneth Rangnes <ok@rait.no>
2013-03-16 20:58:14 +0100

    Changed config
    Added line numbers, reverting unchanged buffers when file changes on
    disk, untabifying buffers on save, removed compile-on-save in PHP

--- init.el
+++ init.el
@@ -32,6 +32,38 @@
 ;; enable column-number-mode
 (column-number-mode t)
 
+;; show paren mode
+(show-paren-mode t)
+
+;; show line numbers on the side
+(global-linum-mode t)
+
+(defvar my-linum-format-string "%3d")
+
+(add-hook 'linum-before-numbering-hook 'my-linum-get-format-string)
+
+(defun my-linum-get-format-string ()
+  (let* ((width (1+ (length (number-to-string
+                             (count-lines (point-min) (point-max))))))
+         (format (concat "%" (number-to-string width) "d")))
+    (setq my-linum-format-string format)))
+
+(defvar my-linum-current-line-number 0)
+
+(setq linum-format 'my-linum-relative-line-numbers)
+
+(defun my-linum-relative-line-numbers (line-number)
+  (let ((offset (abs (- line-number my-linum-current-line-number))))
+    (propertize (format my-linum-format-string offset) 'face 'linum)))
+
+(defadvice linum-update (around my-linum-update)
+  (let ((my-linum-current-line-number (line-number-at-pos)))
+    ad-do-it))
+(ad-activate 'linum-update)
+
+;; Auto update buffer from file, when not modified
+(global-auto-revert-mode t)
+
 (ido-mode t)
 (setq ido-enable-prefix nil
       ido-enable-flex-matching t
@@ -74,11 +106,16 @@
   (require 'twilight-theme))
 (add-hook 'after-init-hook 'my-after-init)
 
+;; Javascript
+(add-to-list 'auto-mode-alist '(".js$" . js2-mode))
+
 (font-lock-add-keywords
  'js2-mode `(("\\(function *\\)("
               (0 (progn (compose-region (match-beginning 1)
                                         (match-end 1) "f")
                         nil)))))
 
+(require 'misc-func)
 (require 'key-bindings)
 (require 'setup-php)
+(require 'setup-scss)
