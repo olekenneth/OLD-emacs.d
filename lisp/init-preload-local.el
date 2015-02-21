46e7aaccd23d2e9dd328307357b21591bc6ce677
Ole-Kenneth Rangnes <ok@rait.no>
2015-02-21 12:03:09 +0100

    update config to require packages and setup correct theme

---
+++ lisp/init-preload-local.el
@@ -0,0 +1,9 @@
+;;; emacsconfig --- Custom emacs config pre init config
+;;; Commentary:
+;;; Require some packages
+(require-package 'helm)
+(require-package 'helm-projectile)
+
+;;; Code:
+(provide 'init-preload-local)
+;;; init-preload-local.el ends here
