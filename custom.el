75ceec5ad2f4605be5c9e6feac1f2efc7b3eb870
Ole-Kenneth Rangnes <ok@rait.no>
2015-02-22 20:11:53 +0100

    set tab width to 4 spaces in all modes

--- custom.el
+++ custom.el
@@ -5,7 +5,20 @@
 
 ;;; Code:
 (helm-mode 1)
-; (helm-autoresize-mode 1)
+                                        ; (helm-autoresize-mode 1)
+
+(setq indent-tabs-mode nil)
+(setq tab-width 4)
+
+(setq-default indent-tabs-mode nil)
+(setq-default tab-width 4)
+
+(setq sgml-basic-offset 4)
+
+(add-hook 'html-mode-hook
+          (lambda ()
+            (setq sgml-basic-offset 4)
+            (setq indent-tabs-mode nil)))
 
 (defun custom-open-file ()
   "Open file using projectile+Helm or ido."
@@ -21,6 +34,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(js2-basic-offset 4)
  '(custom-enabled-themes (quote (sanityinc-tomorrow-day)))
  '(custom-safe-themes (quote ("bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" "sanityinc-tomorrow-day" default)))
  '(session-use-package t nil (session)))
