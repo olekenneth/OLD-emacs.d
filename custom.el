2c8a5052d2f0811bd854e02fd8008022ebbbd033
Ole-Kenneth Rangnes <ok@vg.no>
2015-03-02 08:07:32 +0100

    enabled display-time-mode and customize it to 24 and workday

--- custom.el
+++ custom.el
@@ -37,7 +37,11 @@
  '(js2-basic-offset 4)
  '(custom-enabled-themes (quote (sanityinc-tomorrow-day)))
  '(custom-safe-themes (quote ("bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" "sanityinc-tomorrow-day" default)))
- '(session-use-package t nil (session)))
+ '(display-time-24hr-format t)
+ '(display-time-default-load-average nil)
+ '(display-time-mode t)
+ '(session-use-package t nil (session))
+ '(timeclock-mode-line-display t nil (timeclock)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
