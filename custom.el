652c99b18663de6fd030d5c5faa234ff805bc8df
Ole-Kenneth Rangnes <ok@vg.no>
2015-08-11 10:10:56 +0200

    Update custom.el

--- custom.el
+++ custom.el
@@ -34,14 +34,17 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(js2-basic-offset 4)
  '(custom-enabled-themes (quote (sanityinc-tomorrow-day)))
  '(custom-safe-themes (quote ("bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" "sanityinc-tomorrow-day" default)))
  '(display-time-24hr-format t)
  '(display-time-default-load-average nil)
  '(display-time-mode t)
+ '(js2-basic-offset 4)
+ '(newsticker-url-list (quote (("VG" "http://www.vg.no/rss/feed/forsiden?sources=vg&insiderimage=1" nil 3600 nil))))
  '(session-use-package t nil (session))
- '(timeclock-mode-line-display t nil (timeclock)))
+ '(timeclock-mode-line-display t nil (timeclock))
+ '(timeclock-relative nil)
+ '(timeclock-workday 28801))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
